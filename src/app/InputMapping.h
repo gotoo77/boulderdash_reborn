52839b8a4260bffde315bcb45950c6c0117b52f0
gotoo <gautier.duval@gmail.com>
2026-08-12 21:32:50 +0200

    Fix gamepad event lifecycle outside SDL filter

--- src/app/InputMapping.h
+++ src/app/InputMapping.h
@@ -9,6 +9,10 @@
 
 inline constexpr SDL_Keycode DefaultFullscreenKey = SDLK_f;
 
+void initializeGamepadInput();
+void shutdownGamepadInput();
+bool translateGamepadEvent(SDL_Event& event);
+
 std::optional<Direction> directionFromKey(SDL_Keycode key);
 SDL_Keycode parseKeyFromName(const std::string& name, SDL_Keycode fallback, const char* context);
 bool isVolumeIncreaseKey(SDL_Keycode key);
